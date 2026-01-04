5264def164bddc10f4dffe33d0d5586eb2230b15
otofuserver <otouhusan.live@gmail.com>
2026-01-05 01:02:37 +0900

    fix: 例外クラスに serialVersionUID を追加してシリアライズ警告を解消

--- src/main/java/com/edamame/web/exception/AdminRetentionException.java
+++ src/main/java/com/edamame/web/exception/AdminRetentionException.java
@@ -1,11 +1,32 @@
 package com.edamame.web.exception;
 
+import java.io.Serial;
+
 /**
  * 最後の admin アカウントを無効化/削除しようとした場合にスローする例外
  */
+@SuppressWarnings("unused")
 public class AdminRetentionException extends RuntimeException {
+    // シリアライズ互換性のための識別子
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * デフォルトコンストラクタ
+     * シリアライズ/デシリアライズやフレームワークでの利用のために残すこと
+     */
     public AdminRetentionException() { super(); }
+
+    /**
+     * 指定したメッセージで例外を作成します
+     * @param message 例外メッセージ
+     */
     public AdminRetentionException(String message) { super(message); }
+
+    /**
+     * 指定したメッセージと原因で例外を作成します
+     * @param message 例外メッセージ
+     * @param cause 原因となった例外
+     */
     public AdminRetentionException(String message, Throwable cause) { super(message, cause); }
 }
-
--- src/main/java/com/edamame/web/exception/DuplicateResourceException.java
+++ src/main/java/com/edamame/web/exception/DuplicateResourceException.java
@@ -1,11 +1,34 @@
 package com.edamame.web.exception;
 
+import java.io.Serial;
+
 /**
  * リソースが既に存在する場合にスローするランタイム例外
  */
 public class DuplicateResourceException extends RuntimeException {
+    // シリアライズ互換性のための識別子
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * デフォルトコンストラクタ
+     * シリアライズ/デシリアライズやフレームワークでの利用のために残すこと
+     */
+    @SuppressWarnings("unused")
     public DuplicateResourceException() { super(); }
+
+    /**
+     * 指定したメッセージで例外を作成します
+     * @param message 例外メッセージ
+     */
+    @SuppressWarnings("unused")
     public DuplicateResourceException(String message) { super(message); }
+
+    /**
+     * 指定したメッセージと原因で例外を作成します
+     * @param message 例外メッセージ
+     * @param cause 原因となった例外
+     */
+    @SuppressWarnings("unused")
     public DuplicateResourceException(String message, Throwable cause) { super(message, cause); }
 }
-
